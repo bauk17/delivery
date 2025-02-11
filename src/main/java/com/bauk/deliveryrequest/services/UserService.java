49ec927680a870ad91d72c8827d1521c4d6a7d29
Cauã Pereira <cauapereira_@hotmail.com>
2025-02-11 17:29:21 -0300

    Method createUser implemented

--- src/main/java/com/bauk/deliveryrequest/services/UserService.java
+++ src/main/java/com/bauk/deliveryrequest/services/UserService.java
@@ -1,8 +1,13 @@
 package com.bauk.deliveryrequest.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.bauk.deliveryrequest.dto.UserDto;
+import com.bauk.deliveryrequest.dto.UserResponseDTO;
 import com.bauk.deliveryrequest.models.User;
 import com.bauk.deliveryrequest.repositories.UserRepository;
 
@@ -11,7 +16,22 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public User createUser(User obj) {
-        return userRepository.insert(obj);
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public Optional<User> findUserById(String id) {
+        return userRepository.findById(id);
+    }
+
+    public UserResponseDTO createUser(UserDto userDto) {
+        User user = new User();
+        user.setEmail(userDto.getEmail());
+        user.setName(userDto.getName());
+        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
+        user.setId(userDto.getId());
+
+        User savedUser = userRepository.save(user);
+
+        return new UserResponseDTO(savedUser);
     }
 }
