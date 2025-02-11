8397dce6c9970ad1add23e4bcb0264809c858f47
Cauã Pereira <cauapereira_@hotmail.com>
2025-02-11 17:31:45 -0300

    create user method setted up to run on post /users

---
+++ src/main/java/com/bauk/deliveryrequest/controllers/UserController.java
@@ -0,0 +1,26 @@
+package com.bauk.deliveryrequest.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bauk.deliveryrequest.dto.UserDto;
+import com.bauk.deliveryrequest.dto.UserResponseDTO;
+import com.bauk.deliveryrequest.services.UserService;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponseDTO> createUser(@RequestBody UserDto userDto) {
+        UserResponseDTO newUser = userService.createUser(userDto);
+
+        return ResponseEntity.ok(newUser);
+    }
+}
