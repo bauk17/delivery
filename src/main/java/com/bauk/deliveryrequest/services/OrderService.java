410a813362f63a34e1057b1d87d29bf33c0d9a7a
Cauã Pereira <cauapereira_@hotmail.com>
2025-02-17 20:34:35 -0300

    Method createOrder implemented

--- src/main/java/com/bauk/deliveryrequest/services/OrderService.java
+++ src/main/java/com/bauk/deliveryrequest/services/OrderService.java
@@ -1,5 +1,26 @@
 package com.bauk.deliveryrequest.services;
 
-public class OrderService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.bauk.deliveryrequest.dto.OrderDto;
+import com.bauk.deliveryrequest.models.Order;
+import com.bauk.deliveryrequest.models.User;
+import com.bauk.deliveryrequest.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+    @Autowired
+    private OrderRepository orderRepository;
+    User customer = new User(null, "cauapereira_@hotmail.com", "randombullshit", true, "Cauã");
+
+    public OrderDto createOrder(OrderDto orderDto) {
+        Order userOrder = new Order();
+        userOrder.setCustomer(customer);
+        userOrder.setOrder_date(orderDto.getOrder_date());
+        userOrder.setQuantity(orderDto.getQuantity());
+        Order prevOrder = orderRepository.save(userOrder);
+
+        return new OrderDto(prevOrder);
+    }
 }
