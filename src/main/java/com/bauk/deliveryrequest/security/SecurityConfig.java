ebb5c863d875f8db0472f7095cba4d509beba24e
Cauã Pereira <cauapereira_@hotmail.com>
2025-02-17 20:33:14 -0300

    feat: method securityFilterChain implemented

--- src/main/java/com/bauk/deliveryrequest/security/SecurityConfig.java
+++ src/main/java/com/bauk/deliveryrequest/security/SecurityConfig.java
@@ -3,11 +3,15 @@ package com.bauk.deliveryrequest.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -17,12 +21,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/users").permitAll()
-                        .anyRequest().authenticated());
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(
+                authorize -> authorize.requestMatchers("/users/auth/**").permitAll().anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
+
     }
 }
