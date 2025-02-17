b48bda4f12d5a5ffe094a321796ba5c50cc290f5
Cauã Pereira <cauapereira_@hotmail.com>
2025-02-17 20:22:13 -0300

    Feat: Implements method findByNameOrEmail

--- src/main/java/com/bauk/deliveryrequest/repositories/UserRepository.java
+++ src/main/java/com/bauk/deliveryrequest/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.bauk.deliveryrequest.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.bauk.deliveryrequest.models.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByNameOrEmail(String name, String email);
 }
