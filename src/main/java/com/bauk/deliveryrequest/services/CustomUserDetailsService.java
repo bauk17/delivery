9d4e6661f99bcf26cb1d9575bce46e5dde7bb86f
Cauã Pereira <cauapereira_@hotmail.com>
2025-02-23 16:37:55 -0300

    feat: add CustomUserDetailsService for loading user details by username or email

---
+++ src/main/java/com/bauk/deliveryrequest/services/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.bauk.deliveryrequest.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.bauk.deliveryrequest.models.User;
+import com.bauk.deliveryrequest.repositories.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+
+        User user = userRepository.findByNameOrEmail(usernameOrEmail, usernameOrEmail);
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getName())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
