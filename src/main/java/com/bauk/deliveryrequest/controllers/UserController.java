675dc219c6132c7af72ae958091990e3f7fc33e1
Cauã Pereira <cauapereira_@hotmail.com>
2025-02-23 16:23:44 -0300

    refactor: now the endpoint /auth returns an AuthResponseDTO instead of an UserResponseDTO

--- src/main/java/com/bauk/deliveryrequest/controllers/UserController.java
+++ src/main/java/com/bauk/deliveryrequest/controllers/UserController.java
@@ -1,13 +1,13 @@
 package com.bauk.deliveryrequest.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bauk.deliveryrequest.dto.AuthResponseDTO;
 import com.bauk.deliveryrequest.dto.UserDto;
 import com.bauk.deliveryrequest.dto.UserResponseDTO;
 import com.bauk.deliveryrequest.services.UserService;
@@ -26,12 +26,8 @@ public class UserController {
     }
 
     @PostMapping("/auth")
-    public ResponseEntity<?> authUser(@RequestBody UserDto userDto) {
-        try {
-            UserResponseDTO user = userService.authUser(userDto);
-            return ResponseEntity.ok(user);
-        } catch (RuntimeException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
-        }
+    public ResponseEntity<AuthResponseDTO> authUser(@RequestBody UserDto userDto) {
+        AuthResponseDTO response = userService.authUser(userDto);
+        return ResponseEntity.ok(response);
     }
 }
