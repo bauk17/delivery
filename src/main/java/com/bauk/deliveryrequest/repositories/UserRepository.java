82f607163f14f6a1bce51184eb84c754c07f8ad4
Cauã Pereira <cauapereira_@hotmail.com>
2025-02-23 16:38:37 -0300

    refactor: change findByNameOrEmail method to return User instead of Optional<User>

--- src/main/java/com/bauk/deliveryrequest/repositories/UserRepository.java
+++ src/main/java/com/bauk/deliveryrequest/repositories/UserRepository.java
@@ -1,7 +1,5 @@
 package com.bauk.deliveryrequest.repositories;
 
-import java.util.Optional;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +7,5 @@ import com.bauk.deliveryrequest.models.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
-    Optional<User> findByNameOrEmail(String name, String email);
+    User findByNameOrEmail(String name, String email);
 }
