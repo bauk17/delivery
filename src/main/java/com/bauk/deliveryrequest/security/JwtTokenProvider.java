4453c593e5434e8004ffd49d1775573385ff82b0
Cauã Pereira <cauapereira_@hotmail.com>
2025-02-23 16:36:21 -0300

    feat: implements JwtTokenProvider.java to jwt token generation and validation
    generates jwt tokens to authenticated users
    
    extracts useful informations from toke, like username and authorities
    
    validate the token signature and expiration

---
+++ src/main/java/com/bauk/deliveryrequest/security/JwtTokenProvider.java
@@ -0,0 +1,53 @@
+package com.bauk.deliveryrequest.security;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtTokenProvider {
+    Dotenv dotenv = Dotenv.load();
+    private final String JWT_KEY = dotenv.get("SECRET_KEY");
+    private final long EXPIRATION_TIME = 1000 * 60 * 60 * 60 * 30;
+    private final Key key = Keys.hmacShaKeyFor(JWT_KEY.getBytes());
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder().setSubject(userDetails.getClass().getName())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key, SignatureAlgorithm.HS256).compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractClaim(token, Claims::getExpiration).before(new Date());
+    }
+
+    private Claims extractAlLClaims(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAlLClaims(token);
+        return claimsResolver.apply(claims);
+
+    }
+
+}
