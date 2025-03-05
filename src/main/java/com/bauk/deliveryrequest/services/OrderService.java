db26f23168e5c8ff6a14711a27ed4f8c7f91f6fb
Cauã Pereira <cauapereira_@hotmail.com>
2025-03-04 22:44:54 -0300

    feat: enhance OrderService to retrieve user details from security context and set UserResponseDTO as customer in order creation

--- src/main/java/com/bauk/deliveryrequest/services/OrderService.java
+++ src/main/java/com/bauk/deliveryrequest/services/OrderService.java
@@ -1,22 +1,51 @@
 package com.bauk.deliveryrequest.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import com.bauk.deliveryrequest.dto.OrderDto;
+import com.bauk.deliveryrequest.dto.UserResponseDTO;
+import com.bauk.deliveryrequest.exceptions.ObjectNotFoundException;
 import com.bauk.deliveryrequest.models.Order;
 import com.bauk.deliveryrequest.models.User;
 import com.bauk.deliveryrequest.repositories.OrderRepository;
+import com.bauk.deliveryrequest.repositories.UserRepository;
+import com.bauk.deliveryrequest.security.JwtTokenProvider;
 
 @Service
 public class OrderService {
+
     @Autowired
     private OrderRepository orderRepository;
-    User customer = new User(null, "cauapereira_@hotmail.com", "randombullshit", true, "Cauã");
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
 
     public OrderDto createOrder(OrderDto orderDto) {
+
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username;
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails) principal).getUsername();
+        } else {
+            username = principal.toString();
+        }
+
+        User user = userRepository.findByNameOrEmail(username, username);
+
+        if (user == null) {
+            throw new ObjectNotFoundException("User not found");
+        }
+
+        UserResponseDTO userOrdering = new UserResponseDTO(user);
+
         Order userOrder = new Order();
-        userOrder.setCustomer(customer);
+        userOrder.setCustomer(userOrdering);
         userOrder.setOrder_date(orderDto.getOrder_date());
         userOrder.setQuantity(orderDto.getQuantity());
         Order prevOrder = orderRepository.save(userOrder);
