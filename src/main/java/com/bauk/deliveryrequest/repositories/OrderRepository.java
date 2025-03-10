f0fe78507ed4e4311ee2e8015884204e6b63263a
Cauã Pereira <cauapereira_@hotmail.com>
2025-03-09 22:58:32 -0300

    feat: add method to find orders by customer ID in OrderRepository

--- src/main/java/com/bauk/deliveryrequest/repositories/OrderRepository.java
+++ src/main/java/com/bauk/deliveryrequest/repositories/OrderRepository.java
@@ -1,5 +1,7 @@
 package com.bauk.deliveryrequest.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.bauk.deliveryrequest.models.Order;
 
 @Repository
 public interface OrderRepository extends MongoRepository<Order, String> {
+    List<Order> findByCustomerId(String customerId);
 }
