73cb91d7de3fea04ab2e95188f54f965b60a8618
Cauã Pereira <cauapereira_@hotmail.com>
2025-03-12 20:19:42 -0300

    feat: add SecurityUtil class for retrieving authenticated username

---
+++ src/main/java/com/bauk/deliveryrequest/security/SecurityUtil.java
@@ -0,0 +1,20 @@
+package com.bauk.deliveryrequest.security;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityUtil {
+
+    private SecurityUtil() {
+    }
+
+    public static String getAuthenticatedUsername() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            return ((UserDetails) principal).getUsername();
+        }
+        return principal.toString();
+    }
+}
