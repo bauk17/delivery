ae8ce9a32e20635b3d4c81c408caccad584b6590
Cauã Pereira <cauapereira_@hotmail.com>
2025-03-26 20:45:43 -0300

    feat: enhance OrderController with Swagger annotations and improve error handling

--- src/main/java/com/bauk/deliveryrequest/controllers/OrderController.java
+++ src/main/java/com/bauk/deliveryrequest/controllers/OrderController.java
@@ -18,12 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.bauk.deliveryrequest.dto.OrderDto;
 import com.bauk.deliveryrequest.dto.StatusUpdateRequest;
+import com.bauk.deliveryrequest.exceptions.ObjectNotFoundException;
 import com.bauk.deliveryrequest.models.Order;
 import com.bauk.deliveryrequest.repositories.OrderRepository;
 import com.bauk.deliveryrequest.services.OrderService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/order")
+@Tag(name = "Order", description = "Order API")
 public class OrderController {
     @Autowired
     private OrderService orderService;
@@ -31,27 +37,38 @@ public class OrderController {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Operation(summary = "Create a new order")
+    @ApiResponse(responseCode = "200", description = "Returns an OrderDto")
+    @ApiResponse(responseCode = "409", description = "User not found")
     @PostMapping
     public ResponseEntity<OrderDto> createOrder(@RequestBody OrderDto orderDto) {
         OrderDto newOrder = orderService.createOrder(orderDto);
         return ResponseEntity.ok(newOrder);
     }
 
+    @Operation(summary = "Get all orders")
+    @ApiResponse(responseCode = "200", description = "Returns a list of Orders")
     @GetMapping
     public ResponseEntity<List<Order>> findAllOrders() {
         return ResponseEntity.ok(orderRepository.findAll());
     }
 
+    @Operation(summary = "Get an order by id")
+    @ApiResponse(responseCode = "200", description = "Returns an Order")
+    @ApiResponse(responseCode = "404", description = "Order not found")
     @GetMapping("/{orderId}")
     public ResponseEntity<Order> findOrder(@PathVariable String orderId) {
         Optional<Order> order = orderService.findOrderById(orderId);
         if (order.isPresent()) {
             return ResponseEntity.ok().body(order.get());
         } else {
-            return ResponseEntity.notFound().build();
+            throw new ObjectNotFoundException("Order not found");
         }
     }
 
+    @Operation(summary = "Update an order status")
+    @ApiResponse(responseCode = "200", description = "Returns an Order")
+    @ApiResponse(responseCode = "404", description = "Order not found")
     @PatchMapping("/{orderId}/status")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Order> updateOrder(@PathVariable String orderId, @RequestBody StatusUpdateRequest status)
@@ -61,10 +78,13 @@ public class OrderController {
             Order acceptedOrder = orderService.updateOrderStatus(orderId, status);
             return ResponseEntity.ok().body(acceptedOrder);
         } else {
-            return ResponseEntity.notFound().build();
+            throw new ObjectNotFoundException("Order not found");
         }
     }
 
+    @Operation(summary = "Delete an order")
+    @ApiResponse(responseCode = "204", description = "No content")
+    @ApiResponse(responseCode = "404", description = "Order not found with id {orderId}")
     @DeleteMapping("/{orderId}")
     public ResponseEntity<Void> deleteOrder(@PathVariable String orderId) {
         orderService.deleteOrder(orderId);
