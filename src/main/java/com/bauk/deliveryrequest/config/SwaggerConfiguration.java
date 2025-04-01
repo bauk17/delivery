c88dfc105d1036f1df957f5ca4e650ba127531e6
Cauã Pereira <cauapereira_@hotmail.com>
2025-03-31 23:23:01 -0300

    feat: enhance Swagger configuration with server details and security scheme for JWT authentication

--- src/main/java/com/bauk/deliveryrequest/config/SwaggerConfiguration.java
+++ src/main/java/com/bauk/deliveryrequest/config/SwaggerConfiguration.java
@@ -3,18 +3,29 @@ package com.bauk.deliveryrequest.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfiguration {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI().info(
-                new Info().title("Delivery Order API").version("1.0.0").description("API documentation using OpenAPI")
-                        .contact(new Contact().name("Cauã Pereira").email("cauapereira_@hotmail.com")
-                                .url("https://github.com/bauk17")));
-    }
+        @Bean
+        public OpenAPI customOpenAPI() {
+                return new OpenAPI()
+                                .addServersItem(new Server().url("http://localhost:8080").description("Local Server"))
+                                .info(
+                                                new Info().title("Delivery Order API").version("1.0.0")
+                                                                .description("API documentation using OpenAPI")
+                                                                .contact(new Contact().name("Cauã Pereira")
+                                                                                .email("cauapereira_@hotmail.com")
+                                                                                .url("https://github.com/bauk17")))
+                                .components(new Components().addSecuritySchemes("BearerAuth",
+                                                new SecurityScheme().name("BearerAuth")
+                                                                .type(SecurityScheme.Type.HTTP).scheme("bearer")
+                                                                .bearerFormat("JWT")));
+        }
 }
