b1fa178c595a36cbaf52708248ce1d7a76f6b316
Cauã Pereira <cauapereira_@hotmail.com>
2025-03-31 23:23:53 -0300

    feat: add isAdmin field to UserResponseDTO for enhanced user role representation

--- src/main/java/com/bauk/deliveryrequest/dto/UserResponseDTO.java
+++ src/main/java/com/bauk/deliveryrequest/dto/UserResponseDTO.java
@@ -6,17 +6,20 @@ public class UserResponseDTO {
     private String name;
     private String email;
     private String id;
+    private Boolean isAdmin;
 
-    public UserResponseDTO(String name, String email, String id) {
+    public UserResponseDTO(String name, String email, String id, boolean isAdmin) {
         this.name = name;
         this.email = email;
         this.id = id;
+        this.isAdmin = isAdmin;
     }
 
     public UserResponseDTO(User user) {
         this.email = user.getEmail();
         this.id = user.getId();
         this.name = user.getName();
+        this.isAdmin = user.getIsAdmin();
     }
 
     public String getName() {
@@ -29,6 +32,10 @@ public class UserResponseDTO {
 
     public String getId() {
         return id;
+    }
+
+    public Boolean getIsAdmin() {
+        return isAdmin;
     }
 
 }
