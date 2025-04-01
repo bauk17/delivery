eafa0a6f697a5f1ba51d08115f5d1ea68c5fba0e
Cauã Pereira <cauapereira_@hotmail.com>
2025-03-31 23:23:43 -0300

    feat: remove id field from UserDto for cleaner design and initialization

--- src/main/java/com/bauk/deliveryrequest/dto/UserDto.java
+++ src/main/java/com/bauk/deliveryrequest/dto/UserDto.java
@@ -3,27 +3,21 @@ package com.bauk.deliveryrequest.dto;
 import com.bauk.deliveryrequest.models.User;
 
 public class UserDto {
-    private String id;
+
     private String name;
     private String email;
     private String password;
 
     public UserDto() {
-        this.id = null;
         this.name = null;
         this.email = null;
         this.password = null;
     }
 
     public UserDto(User obj) {
-        this.id = obj.getId();
         this.name = obj.getName();
         this.email = obj.getEmail();
         this.password = obj.getPassword();
-    }
-
-    public String getId() {
-        return id;
     }
 
     public String getName() {
