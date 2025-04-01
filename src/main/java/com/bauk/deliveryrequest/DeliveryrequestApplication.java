7314c2dd598421ec4389409c4b92b209e3e9f64e
Cauã Pereira <cauapereira_@hotmail.com>
2025-04-01 19:54:49 -0300

    feat: load environment variables using Dotenv for improved configuration management

--- src/main/java/com/bauk/deliveryrequest/DeliveryrequestApplication.java
+++ src/main/java/com/bauk/deliveryrequest/DeliveryrequestApplication.java
@@ -3,10 +3,17 @@ package com.bauk.deliveryrequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class DeliveryrequestApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+
+		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+
 		SpringApplication.run(DeliveryrequestApplication.class, args);
 	}
 
